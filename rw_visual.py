b5025ef03739ead39b6bc493dcb2ba7416f5ebc5
Daniel <45560852+Fenrow@users.noreply.github.com>
2019-01-04 11:31:35 +0100

    błądzenie losowe

--- rw_visual.py
+++ rw_visual.py
@@ -2,9 +2,26 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-#przygotowanie danych błądzenia losowego i wyświetlanie punktów
-rw = RandomWalk(500000)
-rw.fill_walk()
+while True:
+    #przygotowanie danych błądzenia losowego i wyświetlanie punktów
+    rw = RandomWalk(50000)
+    rw.fill_walk()
 
-plt.scatter(rw.x_values, rw.y_values, s = 5)
-plt.show()
+    #Określenie wielkości okna wykresu
+    plt.figure(figsize=(10, 6))
+
+    point_numbers = list(range(rw.num_points))
+
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+    plt.scatter(0, 0, c='green', edgecolors='none', s=20) #Wyświetlenie punktu początkowego
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=20) #Wyświetlenie punktu końcowego
+
+    #Ukrycie osi
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input('Utworzyć kolejne błądzenie losowe? (t/n): ')
+    if keep_running.lower() == 'n':
+        break
